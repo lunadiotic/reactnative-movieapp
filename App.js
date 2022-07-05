bbe56b54004761a8d70945cd058bbf5587bfa31c
IDStack <idstack@outlook.co.id>
2022-07-06 00:16:24 +0700

    using stylesheet property separately

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Home from './screens/Home';
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
+    <View style={styles.container}>
       <Home />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
