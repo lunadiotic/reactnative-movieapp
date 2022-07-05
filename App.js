2a7001688f6237878a73073012d3599cf9140ddf
IDStack <idstack@outlook.co.id>
2022-07-05 23:33:49 +0700

    populate movie from api

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import axios from 'axios';
+
+const getPopMovies = async () => {
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/movie/popular?api_key=778306490922af732ee8ce5aeb1ef02e',
+  );
+  console.log(JSON.stringify(response.data.results[0], null, 2));
+};
 
 const App = () => {
-  console.log('test');
+  getPopMovies();
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Try editing me! 🎉</Text>
