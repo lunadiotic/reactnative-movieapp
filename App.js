4979c3cd0773811f902e26ef16d678059c6c3a04
IDStack <idstack@outlook.co.id>
2022-07-05 23:34:47 +0700

    display movie data to component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View} from 'react-native';
 import axios from 'axios';
 
@@ -6,14 +6,27 @@ const getPopMovies = async () => {
   const response = await axios.get(
     'https://api.themoviedb.org/3/movie/popular?api_key=778306490922af732ee8ce5aeb1ef02e',
   );
-  console.log(JSON.stringify(response.data.results[0], null, 2));
+  return response.data.results;
 };
 
 const App = () => {
-  getPopMovies();
+  const [movie, setMovie] = useState({});
+  getPopMovies()
+    .then(result => {
+      setMovie(result[0]);
+    })
+    .catch(err => {});
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Try editing me! 🎉</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>{movie.original_title}</Text>
+      <Text>{movie.original_language}</Text>
+      <Text>{movie.release_date}</Text>
     </View>
   );
 };
