115f125392dc40a062ecf81f86ee7f709ef63de5
IDStack <idstack@outlook.co.id>
2022-07-06 09:10:00 +0700

    show image slide from api

--- screens/Home.js
+++ screens/Home.js
@@ -2,13 +2,29 @@
 
 import React, {useEffect, useState} from 'react';
 import {Text} from 'react-native';
-import {getPopularMovies} from '../services/api-request';
+import {getPopularMovies, getUpcomingMovies} from '../services/api-request';
+import {SliderBox} from 'react-native-image-slider-box';
 
 const Home = () => {
   const [movie, setMovie] = useState('');
+  const [movieImages, setMovieImages] = useState([]);
   const [err, setErr] = useState(false);
 
   useEffect(() => {
+    getUpcomingMovies()
+      .then(movies => {
+        let images = [];
+        for (let movie = 0; movie < movies.length; movie++) {
+          images.push(
+            `https://image.tmdb.org/t/p/w500${movies[movie]['poster_path']}`,
+          );
+        }
+        setMovieImages(images);
+      })
+      .catch(err => {
+        setErr(err);
+      });
+
     getPopularMovies()
       .then(movies => {
         setMovie(movies[0]);
@@ -19,6 +35,7 @@ const Home = () => {
   }, []);
   return (
     <>
+      <SliderBox images={movieImages} />
       <Text>Title: {movie.original_title}</Text>
       <Text>Release Date: {movie.release_date}</Text>
       {err && <Text style={{color: 'red'}}>Internal Server Error</Text>}
